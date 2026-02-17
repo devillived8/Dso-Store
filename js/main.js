9751b76c79b0dbfccb112dfd44ab4bc484552385
devillived8 <il.volobuev2014@yandex.ru>
2026-02-17 20:24:14 +0300

    feat: add guarantees page and fix minor bugs on index

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ import { renderCatalog, renderAccount } from './card.js';
 import { burgerOpen } from './burger.js';
 import {showModal, closeModal} from './modalLogin.js';
 
-
+ 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+
+    burgerOpen();
+    showModal();
+    closeModal();
   const container = document.querySelector('.catalog__main');
   if (container) {
     renderCatalog(container);
     renderAccount();
-    burgerOpen();
-    showModal();
-    closeModal();
+ 
   }
 });
