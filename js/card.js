2554189b41de81b1bf238f038c9251e61475e652
devillived8 <il.volobuev2014@yandex.ru>
2026-02-18 15:57:34 +0300

    feat: enhance cards with adaptive design, price format and buttons

--- js/card.js
+++ js/card.js
@@ -1,5 +1,6 @@
 let catalogMain = document.querySelector(".catalog__main");
 
+// Моковые данные для карточек
 const cardsData = [
   {
     title: "Маг 80 уровня",
@@ -11,71 +12,95 @@ const cardsData = [
   {
     title: "Воин 90 уровня",
     price: "20000р",
-    class: "warrior",
+    class: "ranger",
     image: "./img/mage2.jpg",
     server: "grimmage",
   },
-    {
+  {
+    title: "Воин 90 уровня",
+    price: "20000р",
+    class: "dwarf",
+    image: "./img/mage2.jpg",
+    server: "grimmage",
+  },
+  {
     title: "Воин 90 уровня",
     price: "20000р",
     class: "warrior",
     image: "./img/mage2.jpg",
     server: "grimmage",
   },
-    {
+  {
     title: "Воин 90 уровня",
     price: "20000р",
     class: "warrior",
     image: "./img/mage2.jpg",
     server: "grimmage",
   },
-    {
-    title: "Воин 90 уровня",
-    price: "20000р",
-    class: "warrior",
-    image: "./img/mage2.jpg",
-    server: "grimmage",
-  },
- 
 ];
 
+// Форматирование цены для карточек
+function formatPrice(price) {
+  let numberPart = price.replace("р", "");
+  let length = numberPart.length;
+
+  switch (length) {
+    case 4:
+      return numberPart[0] + "." + numberPart.slice(1) + "р";
+
+    case 5:
+      return numberPart.slice(0, 2) + "." + numberPart.slice(2) + "р";
+
+    default:
+      return price;
+  }
+}
+
+// Создание карточек
 function createCard(cardData) {
+  const formattedPrice = formatPrice(cardData.price);
+  const cardClass = `card card--${cardData.class}`;
   return `
-    <div class="card">
+    <div class="${cardClass}">
       <img src="${cardData.image}" class="card__img" alt="${cardData.title}">
       <h2 class="card__title">${cardData.title}</h2>
       <p class="card__text card__class">Класс: ${cardData.class}</p>
       <p class="card__text card__server">Сервер: ${cardData.server}</p>
-      <p class="card__text card__price">${cardData.price}</p>
+      <div class="card__price-wrapper">
+        <p class="card__text card__price">${formattedPrice}</p>
+        <button class="card__text card__btn">Подробнее</button>
+      </div>
     </div>
   `;
 }
 
+
+
+// Вставка карточек на страницу
 export function renderCatalog() {
   cardsData.forEach((card) => {
     catalogMain.insertAdjacentHTML("beforeend", createCard(card));
   });
 }
 
+// Создание страниц для карточек, при клике по ним
 export function renderAccount() {
   let catalogMain = document.querySelector(".catalog__main");
   catalogMain.addEventListener("click", (e) => {
-      if (e.target.tagName == "IMG") {
-      let infoAccount =  e.target.closest(".card");
-      if(infoAccount){
-        let title = infoAccount.querySelector('.card__title').textContent;
-        let className = infoAccount.querySelector('.card__class').textContent;
-        let server = infoAccount.querySelector('.card__server').textContent;
-        let price = infoAccount.querySelector('.card__price').textContent;
-        let img = infoAccount.querySelector('.card__img').src;
+    if (e.target.tagName == "IMG" || e.target.tagName == "BUTTON") {
+      let infoAccount = e.target.closest(".card");
+      if (infoAccount) {
+        let title = infoAccount.querySelector(".card__title").textContent;
+        let className = infoAccount.querySelector(".card__class").textContent;
+        let server = infoAccount.querySelector(".card__server").textContent;
+        let price = infoAccount.querySelector(".card__price").textContent;
+        let img = infoAccount.querySelector(".card__img").src;
 
-        let info = {title, className, server, price, img};
-        
-        sessionStorage.setItem('account', JSON.stringify(info));
-        window.location.href = 'account.html';
+        let info = { title, className, server, price, img };
 
+        sessionStorage.setItem("account", JSON.stringify(info));
+        window.location.href = "account.html";
       }
-  };
+    }
   });
-
 }
